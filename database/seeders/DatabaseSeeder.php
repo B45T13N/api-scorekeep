41960547d055cb48aa9b7f9ce2e2c1bfcac405a3
B45T13N <bastds.dev@gmail.com>
2023-06-07 11:42:54 +0200

    Update seeder to add LocalTeam

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Game;
+use App\Models\LocalTeam;
 use App\Models\RoomManager;
 use App\Models\Secretary;
 use App\Models\Timekeeper;
@@ -23,11 +24,12 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        LocalTeam::factory(1)->create();
+        VisitorTeam::factory(15)->create();
         Game::factory(10)->create();
 
         RoomManager::factory(15)->create();
         Timekeeper::factory(15)->create();
         Secretary::factory(15)->create();
-        VisitorTeam::factory(15)->create();
     }
 }
