5a52e5c51b15d5d31cfb19cc5d2747d73eccd8e0
b45t13n <bastds.dev@gmail.com>
2023-09-11 10:02:27 +0200

    Add email as nullable field

--- database/migrations/2023_05_22_125823_create_room_manager_table.php
+++ database/migrations/2023_05_22_125823_create_room_manager_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('room_managers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
+            $table->string('email')->nullable();
 
             $table->timestamps();
         });
