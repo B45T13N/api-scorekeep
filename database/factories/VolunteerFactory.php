021859a4139af827e0ca08c796597583845f3d36
B45T13N <bastds.dev@gmail.com>
2023-09-29 13:01:57 +0200

    Create volunteer factgory and resource test

--- database/factories/VolunteerFactory.php
+++ database/factories/VolunteerFactory.php
@@ -5,9 +5,9 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Secretary>
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Volunteer>
  */
-class SecretaryFactory extends Factory
+class VolunteerFactory extends Factory
 {
     /**
      * Define the model's default state.
