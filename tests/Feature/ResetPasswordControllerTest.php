47d3dce5f765704d218b112b58b7fdce9c90f373
B45T13N <bastds.dev@gmail.com>
2024-01-21 21:07:26 +0100

    Remvoe warnignsd

--- tests/Feature/ResetPasswordControllerTest.php
+++ tests/Feature/ResetPasswordControllerTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\User;
 use App\Notifications\CustomResetPasswordNotification;
-use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Notification;
